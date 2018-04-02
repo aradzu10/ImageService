e072b99cd86c3b2a086f38e6697213cae631399b
aradzu10 <aradzulti@gmail.com>
2018-04-02 19:59:05 +0300

    first

---
+++ ImageService/ImageService.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+public enum ServiceState
+{
+    SERVICE_STOPPED = 0x00000001,
+    SERVICE_START_PENDING = 0x00000002,
+    SERVICE_STOP_PENDING = 0x00000003,
+    SERVICE_RUNNING = 0x00000004,
+    SERVICE_CONTINUE_PENDING = 0x00000005,
+    SERVICE_PAUSE_PENDING = 0x00000006,
+    SERVICE_PAUSED = 0x00000007,
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public struct ServiceStatus
+{
+    public int dwServiceType;
+    public ServiceState dwCurrentState;
+    public int dwControlsAccepted;
+    public int dwWin32ExitCode;
+    public int dwServiceSpecificExitCode;
+    public int dwCheckPoint;
+    public int dwWaitHint;
+};
+
+
+namespace ImageService
+{
+    public partial class ImageService : ServiceBase
+    {
+        public ImageService()
+        {
+            InitializeComponent();
+            string eventSourceName = "MySource";
+            string logName = "MyNewLog";
+            eventLog = new System.Diagnostics.EventLog();
+            if (!System.Diagnostics.EventLog.SourceExists(eventSourceName))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(eventSourceName, logName);
+            }
+            eventLog.Source = eventSourceName;
+            eventLog.Log = logName;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog.WriteEntry("Service Start Pending.");
+            ServiceStatus serviceStatus = new ServiceStatus();
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_START_PENDING;
+            serviceStatus.dwWaitHint = 100000;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+            // Set up a timer to trigger every minute.  
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_RUNNING;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+            eventLog.WriteEntry("Service Started.");
+        }
+
+        protected override void OnStop()
+        {
+            eventLog.WriteEntry("Service Stoped.");
+        }
+
+        protected override void OnContinue()
+        {
+            eventLog.WriteEntry("Service Running.");
+        }
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool SetServiceStatus(IntPtr handle, ref ServiceStatus serviceStatus);
+    }
+}
