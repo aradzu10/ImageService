8d47da09513301bac9d9ead6bce1ba1661d664a8
aradzu10 <aradzulti@gmail.com>
2018-04-14 18:19:07 +0300

    done - to submit

--- ImageService/Program.cs
+++ ImageService/Program.cs
@@ -1,9 +1,11 @@
 ﻿using ImageService.Controller;
 using ImageService.DirectoryListener;
 using ImageService.FileHandler;
+using ImageService.ListenerManager;
 using Logger;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
@@ -25,6 +27,16 @@ namespace ImageService
                 new ImageService()
             };
             ServiceBase.Run(ServicesToRun);
+            /* my tests
+            ------------
+            string outputFolder = ConfigurationManager.AppSettings.Get("OutputDir");
+            int ThumbnailSize = Int32.Parse(ConfigurationManager.AppSettings.Get("ThumbnailSize"));
+            ILoggingService logger = new LoggingService();
+            ImageListenerManager listenerManager = new ImageListenerManager(logger, outputFolder, ThumbnailSize);
+            string[] folderToListen = (ConfigurationManager.AppSettings.Get("Handler").Split(';'));
+            listenerManager.StartListenDir(folderToListen);
+            while (true) { }
+            */
         }
     }
 }
